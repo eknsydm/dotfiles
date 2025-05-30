039304815c789c83365846768133cb899db18d79
eknsydm <46608164+eknsydm@users.noreply.github.com>
2025-05-30 17:39:40 +0300

    .

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,6 @@
-require("core.options2")
+require("core.options")
 require("core.keymaps")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     local lazyrepo = "https://github.com/folke/lazy.nvim.git"
